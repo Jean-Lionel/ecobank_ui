3a5ac241b1f85e7d207587b4f9e724468bf9f1b8
Jean-Lionel <nijeanlionel@gmail.com>
2022-07-01 05:43:14 +0200

    make autentification

--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ import App from './App';
 //import "currency-flags"
 import "./asset/flag/style.css"
 
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:8000/api';
+//axios.defaults.baseURL = 'http://192.168.0.22:8000/api';
+//axios.defaults.baseURL = 'https://onprapi.herokuapp.com/api';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    //console.log(request);
+    // Edit request config
+    return request;
+}, error => {
+   // console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  //  console.log(response);
+    // Edit response config
+    return response;
+}, error => {
+   // console.log(error);
+    return Promise.reject(error);
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
