799f50314d080fe411fff22d6c9802663735aa07
Jean-Lionel <nijeanlionel@gmail.com>
2022-07-02 20:18:15 +0200

    display list user

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,51 @@
+import { Alert, Table, TableCell, TableRow } from "@mui/material";
+import { useEffect, useState } from "react";
 import Dashboard from "../components/dashboard/Dashboard";
+import useFetchDataWithPagination from "../utility/useFetchDataWithPagination";
+import usePostData from "../utility/usePostData";
 
 const User = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { error, submitData } = usePostData()
+    const { data, resfreshSearch } = useFetchDataWithPagination("users");
+    const [listeUser, setListeUser] = useState(null);
+
+    useEffect(() => {
+        if (data?.data) {
+            setListeUser(data?.data)
+        }
+    }, [data]);
+
+    const saveUser = (e) => {
+        e.preventDefault();
+        const data = { email, name, password };
+        submitData("register", data);
+        resfreshSearch();
+    }
     return (<Dashboard>
+        <form onClick={saveUser}>
+            <input type="text" placeholder="NAME" onChange={(e)=> setName(e.target.value)}/>
+            <input type="text" placeholder="EMAIL" onChange={(e)=> setEmail(e.target.value)}/>
+            <input type="text" placeholder="PASSWORD" onChange={(e) => setPassword(e.target.value)} />
+            <input type="submit" value="Enregistrer" />
+            {error && <Alert>
+                {JSON.stringify(error)}
+            </Alert>}
+        </form>
+
+        <Table>
+            <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>Nom et prénom </TableCell>
+                <TableCell>Email</TableCell>
+            </TableRow>
+
+            {listeUser && <TableRow>
+                {JSON.stringify(listeUser)}
+                </TableRow>}
+        </Table>
     </Dashboard>);
 }
  
